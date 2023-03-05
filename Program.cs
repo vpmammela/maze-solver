51f6678bb24cff7a94e09a74b83923dc1de21c15
Veli-Pekka Mämmelä <vpmammela@icloud.com>
2023-03-05 11:46:58 +0200

    read maze file done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string file;
+        Console.Write("Select maze to solve (1 or 2): ");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                file = "maze-task-first.txt";
+                break;
+            case "2":
+                file = "maze-task-second.txt";
+                break;
+            default:
+                Console.WriteLine("Invalid selection");
+                return;
+        }
+
+        string[] maze = File.ReadAllLines($"./mazes/{file}");
+
+        // print each line of the maze
+        foreach (string line in maze)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
