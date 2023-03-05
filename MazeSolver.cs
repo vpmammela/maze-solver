0288fc6ca85e14831139cbf8110e9db9bcad2aaa
Veli-Pekka Mämmelä <vpmammela@icloud.com>
2023-03-05 12:44:11 +0200

    code refactored

---
+++ MazeSolver.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class MazeSolver
+{
+    private readonly string[] maze;
+    private readonly int startX, startY, endX, endY;
+    private readonly bool[,] visited;
+    private readonly int[] dx = { 1, -1, 0, 0 };
+    private readonly int[] dy = { 0, 0, 1, -1 };
+
+    public MazeSolver(string file)
+    {
+        maze = File.ReadAllLines($"./mazes/{file}");
+
+        for (int i = 0; i < maze.Length; i++)
+        {
+            for (int j = 0; j < maze[i].Length; j++)
+            {
+                if (maze[i][j] == '^')
+                {
+                    startX = i;
+                    startY = j;
+                }
+                else if (maze[i][j] == 'E')
+                {
+                    endX = i;
+                    endY = j;
+                }
+            }
+        }
+
+        if (startX == -1 || startY == -1 || endX == -1 || endY == -1)
+        {
+            throw new InvalidOperationException("Maze does not contain starting and ending positions.");
+        }
+
+        visited = new bool[maze.Length, maze[0].Length];
+    }
+
+    public List<(int x, int y)> FindShortestPath()
+    {
+        Queue<(int x, int y, int moves, List<(int x, int y)> path)> queue = new Queue<(int x, int y, int moves, List<(int x, int y)> path)>();
+        queue.Enqueue((startX, startY, 0, new List<(int x, int y)> { (startX, startY) }));
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+
+            if (current.x == endX && current.y == endY)
+            {
+                return current.path;
+            }
+
+            if (current.moves >= 200)
+            {
+                throw new InvalidOperationException("Maximum number of moves exceeded.");
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int newX = current.x + dx[i];
+                int newY = current.y + dy[i];
+
+                if (newX >= 0 && newX < maze.Length && newY >= 0 && newY < maze[newX].Length && maze[newX][newY] != '#' && !visited[newX, newY])
+                {
+                    visited[newX, newY] = true;
+                    List<(int x, int y)> newPath = new List<(int x, int y)>(current.path);
+                    newPath.Add((newX, newY));
+                    queue.Enqueue((newX, newY, current.moves + 1, newPath));
+                }
+            }
+        }
+
+        throw new InvalidOperationException("No route found.");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 
+
 class Program
 {
     static void Main(string[] args)
     {
         string file;
         Console.Write("Select maze to solve (1 or 2): ");
-        string choice = Console.ReadLine();
+        string choice = Console.ReadLine()!;
         switch (choice)
         {
             case "1":
@@ -24,88 +25,20 @@ class Program
 
         string[] maze = File.ReadAllLines($"./mazes/{file}");
 
+        MazeSolver solver = new MazeSolver(file);
+        
+        List<(int x, int y)> path = solver.FindShortestPath();
 
-        int startX = -1, startY = -1, endX = -1, endY = -1;
-
-        // Find the starting and ending positions in the maze
-        for (int i = 0; i < maze.Length; i++)
+        
+        foreach (var position in path)
         {
-            for (int j = 0; j < maze[i].Length; j++)
-            {
-                if (maze[i][j] == '^')
-                {
-                    startX = i;
-                    startY = j;
-                }
-                else if (maze[i][j] == 'E')
-                {
-                    endX = i;
-                    endY = j;
-                }
-            }
+            maze[position.x] = maze[position.x].Substring(0, position.y) + "@" + maze[position.x].Substring(position.y + 1);
         }
 
-        if (startX == -1 || startY == -1 || endX == -1 || endY == -1)
+        foreach (string line in maze)
         {
-            Console.WriteLine("Error: Maze does not contain starting and ending positions.");
-            return;
+            Console.WriteLine(line);
         }
-
-        bool[,] visited = new bool[maze.Length, maze[0].Length];
-
-        // Define the four possible directions of movement
-        int[] dx = { 1, -1, 0, 0 };
-        int[] dy = { 0, 0, 1, -1 };
-
-        // Use a queue to perform a breadth-first search of the maze
-        Queue<(int x, int y, int moves, List<(int x, int y)> path)> queue = new Queue<(int x, int y, int moves, List<(int x, int y)> path)>();
-        queue.Enqueue((startX, startY, 0, new List<(int x, int y)> { (startX, startY) }));
-
-        bool foundExit = false;
-
-        while (queue.Count > 0)
-        {
-            var current = queue.Dequeue();
-
-            // Check if the current position is the exit
-            if (current.x == endX && current.y == endY)
-            {
-                Console.WriteLine("Found a route in " + current.moves + " moves:");
-                foreach (var position in current.path)
-                {
-                    maze[position.x] = maze[position.x].Substring(0, position.y) + "@" + maze[position.x].Substring(position.y + 1);
-                }
-                foundExit = true;
-                break;
-            }
-
-            // Check if the maximum number of moves has been exceeded
-            if (current.moves >= 200)
-            {
-                Console.WriteLine("Error: Maximum number of moves exceeded.");
-                return;
-            }
-
-            // Check each possible direction from the current position
-            for (int i = 0; i < 4; i++)
-            {
-                int newX = current.x + dx[i];
-                int newY = current.y + dy[i];
-
-                // Check if the new position is within the maze bounds and is not a block
-                if (newX >= 0 && newX < maze.Length && newY >= 0 && newY < maze[newX].Length && maze[newX][newY] != '#' && !visited[newX, newY])
-                {
-                    visited[newX, newY] = true;
-                    List<(int x, int y)> newPath = new List<(int x, int y)>(current.path);
-                    newPath.Add((newX, newY));
-                    queue.Enqueue((newX, newY, current.moves + 1, newPath));
-                }
-            }
-        }
-
-        if (!foundExit)
-        {
-            Console.WriteLine("No route found.");
-        }
+        
     }
 }
